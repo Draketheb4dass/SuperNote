14f8b24ffa5443a5d76785c151bcb50ea019b017
Jephte Drake Colin <drakecolinj@gmail.com>
2018-10-03 18:38:35 -0400

    Add listView
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/supernote/MainActivity.kt
+++ app/src/main/java/com/realty/drake/supernote/MainActivity.kt
@@ -3,11 +3,11 @@ package com.realty.drake.supernote
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
-    internal var lvItems: ListView
+    internal lateinit var lvNote: ListView
     internal var etEditText: EditText
-    private val REQUEST_CODE = 123
     private var mDbHelper: TodoDbHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
